8e57f59801e1f203367945a9ab6d5ce92edc6c3a
Anand.C <122742595+AnandC7github@users.noreply.github.com>
2023-08-22 19:35:45 +0530

    Add files via upload

---
+++ cpp3.cpp
@@ -0,0 +1,21 @@
+//Program to Add Two Integers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int first, second, sum;
+	
+	cout << "Enter the first number";
+	cin >> first;
+	
+	cout << "Enter the second number";
+	cin >> second;
+	
+	sum = first + second;
+	
+	cout << "The sum of " << first << " and " << second << "is " << sum;
+	return 0;
+	
+}

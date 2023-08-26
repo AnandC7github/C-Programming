3e8f1d09d6f3745c8c8c09e53b269cb7d2c0c7ad
Anand.C <122742595+AnandC7github@users.noreply.github.com>
2023-08-26 22:34:26 +0530

    Add files via upload

---
+++ cpp9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,mul;
+	cout << "Enter the first number : ";
+	cin >> a;
+	cout << "Enter the second number : ";
+	cin >> b;
+	mul = a * b;
+	cout << a << " * " << b << " is " << mul << endl;
+	return 0;
+}

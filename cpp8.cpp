a2609e2c53792c8c477246bbab3b1f5c34b5d038
Anand.C <122742595+AnandC7github@users.noreply.github.com>
2023-08-26 22:34:11 +0530

    Add files via upload

---
+++ cpp8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char c;
+	cout << "Enter the charecter : ";
+	cin >> c;
+	cout << "The ascii value of the charecter" << c <<" is " << int(c) ;
+	return 0;
+}
